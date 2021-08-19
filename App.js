7cf4e4ad719419acaad87a15ce1b8209eae47068
Bruce <bruce@redravencomputing.com>
2021-08-18 19:12:53 -0600

    Updated metrics in helpers.

--- App.js
+++ App.js
@@ -1,15 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Testing this</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Ionicons name='ios-pizza' color='red' size={100}/>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
