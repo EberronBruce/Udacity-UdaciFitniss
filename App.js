387383550c05cac22274dbda0a8864be732ee213
Bruce <bruce@redravencomputing.com>
2021-08-21 16:08:03 -0600

    Added an actual slider to AddEntry

--- App.js
+++ App.js
@@ -9,16 +9,7 @@ export default class App extends React.Component {
   render() {
     return(
       <View style={styles.container}>
-        <Slider
-          minimumValue={-10}
-          maximumValue={10}
-          step={1}
-          value={this.state.value}
-          onValueChange={(value) => this.setState(() => ({ value }))}
-        />
-        <Text>
-          Value: {this.state.value}
-        </Text>
+        <AddEntry />
       </View>
     );
   }
