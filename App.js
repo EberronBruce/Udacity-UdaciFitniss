5793e819f4a5f8fd48fcad8171b2e9b3b380a937
Bruce <bruce@redravencomputing.com>
2021-08-21 12:06:35 -0600

    Messing around with touchables.

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, TouchableHighlight, TouchableNativeFeedback, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
+  handlePress = () => {
+    alert('Hello!')
+  }
   render() {
     return(
-      <View>
-        <AddEntry />
+      <View style={styles.container}>
+        <TouchableNativeFeedback
+          background={TouchableNativeFeedback.SelectableBackground()}
+          onPress={this.handlePress}>
+          <View style={styles.btn}>
+            <Text style={styles.btnText}>Touchable Highlight</Text>
+          </View>
+        </TouchableNativeFeedback>
       </View>
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  btn: {
+    backgroundColor: '#E53224',
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  btnText: {
+    color: '#fff'
+  }
+})
