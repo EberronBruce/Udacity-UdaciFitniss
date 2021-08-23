afcb2a32c2981885080f1d8a646863f0ffe32221
Bruce <bruce@redravencomputing.com>
2021-08-22 22:04:36 -0600

    Added styles to AddEntry and Steppers.

--- App.js
+++ App.js
@@ -4,12 +4,12 @@ import AddEntry from './components/AddEntry';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
- 
+
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View sytle={{flex: 1}}>
+        <View style={{flex: 1}}>
           <AddEntry />
         </View>
       </Provider>
